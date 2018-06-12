a674187a81267763aede35f79fa1ffd4c7852bb9
Ping Yang <pingyang@thoughtworks.com>
2018-06-12 23:28:57 +0800

    config proxy.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,12 +5,29 @@
 </template>
 
 <script>
+import axios from 'axios'
+axios.defaults.baseURL = '/api'
+axios.defaults.headers['Content-Type'] = 'application/json;charset=UTF-8'
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  methods: {
+    initData () {
+      axios.get('/object-test')
+        .then(function (respone) {
+          console.log(respone)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    }
+  },
+  created () {
+    this.initData()
   }
 }
 </script>
